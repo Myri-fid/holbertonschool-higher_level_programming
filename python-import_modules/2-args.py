d241d130267905a89eeb916307e0d89dcbeea2cf
Myriam <myriiamwayne@gmail.com>
2025-01-17 23:58:31 +0100

    print list argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,8 @@
 import sys
 
 if __name__ == "__main__":
-    print("{} argument{}{}".format(argc, "s" if argc != 1 else "", "." if argc == 0 else ":"))
+    argc = len(sys.argv) - 1
+    print("{} argument{}".format(argc, "s" if argc != 1 else ""), end="")
+    print("{}".format("." if argc == 0 else ":"))
     for i in range(argc):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
