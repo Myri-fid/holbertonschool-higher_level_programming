1c6184026a639ff202c4ca7f01470b41295e2a73
Myriam <myriiamwayne@gmail.com>
2025-01-17 13:47:53 +0100

    refactor: simplify alphabet printing by using a loop instead of string joining

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-print("".join("{:c}".format(i) for i in range(97, 123)), end="")
-
-# "{:c}".format(i) convertit i en son caractère ASCII
-# "".join(...) caractères générés et concatène en une chaîne.
-# end="" empêche l'ajout d'un saut de ligne à la fin
+for i in range(97, 123):
+    print("{:c}".format(i), end='')
