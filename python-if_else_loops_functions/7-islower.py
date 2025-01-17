6d7eec18f2c9d1195b48e45212302462874574cb
Myriam <myriiamwayne@gmail.com>
2025-01-17 20:09:45 +0100

    refactor: simplify islower function to return boolean value

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord('a') <= ord(c) <= ord('z'):
-        print("{} is lower".format(c))
-    else:
-        print("{} is upper".format(c))
+    return ord(c) >= 97 and ord(c) <= 122
