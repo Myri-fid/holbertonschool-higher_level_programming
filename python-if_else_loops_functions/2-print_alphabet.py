e3d7aae7002ef2e10c9fa13295e9e0842ff8f7f3
Myriam <myriiamwayne@gmail.com>
2025-01-17 13:25:01 +0100

    docs: add comments explaining ASCII conversion and string joining in print_alphabet.py

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 print("".join("{:c}".format(i) for i in range(97, 123)), end="")
+
+# "{:c}".format(i) convertit i en son caractère ASCII
+# "".join(...) caractères générés et concatène en une chaîne.
+# end="" empêche l'ajout d'un saut de ligne à la fin
