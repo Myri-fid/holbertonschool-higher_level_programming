b8da9cc32ceb0322050fe7fc1cc415a78176bec7
Myriam <myriiamwayne@gmail.com>
2025-01-17 19:44:04 +0100

    print lower or upper case

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
     if ord('a') <= ord(c) <= ord('z'):
-        print("is lower")
+        print("{} is lower".format(c))
     else:
-        print("is upper")
+        print("{} is upper".format(c))
