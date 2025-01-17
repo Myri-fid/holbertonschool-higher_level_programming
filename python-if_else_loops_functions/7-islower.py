9134ce0d242c033681a4817d6f11e4d3b8d5e821
Myriam <myriiamwayne@gmail.com>
2025-01-17 18:51:55 +0100

    Update islower function to print boolean result for lowercase check

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
+    if ord('a') <= ord(c) <= ord('z'):
+        print(True)
+    else:
+        print(False)
