46ff6cd913d9d2e940aa5ee822cff5c354a12d4c
Myriam <myriiamwayne@gmail.com>
2025-01-24 21:21:14 +0100

    feat: update list_division function to append results to a new list and return it

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -12,4 +12,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            print(result)
+            newlist.append(result)
+    return newlist
