c9dd21427942420a312a452131b9962ed2194546
Myriam <myriiamwayne@gmail.com>
2025-01-24 13:54:13 +0100

    feat: implement uniq_add function to sum unique elements in a list

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = []
+    for i in my_list:
+        if i not in result:
+            result.append(i)
+    result = sum(result)
+    return result
+ 
