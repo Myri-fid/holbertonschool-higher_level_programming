e1c7ce556d1f917a550ea1f5af3fb0a8cb8f03d4
Myriam <myriiamwayne@gmail.com>
2025-01-24 20:13:07 +0100

    fix: correct safe_print_list_integers function to properly format and print integers from the list

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,9 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
-            print("{:d}".format(), end="")
+            print("{:d}".format(my_list[i]), end="")
             count += 1
-    except IndexError:
+    except ValueError:
         pass
     print()
     return count
