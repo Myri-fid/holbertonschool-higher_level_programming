0d378cddb29b1ec131465eb508338646930cc723
Myriam <myriiamwayne@gmail.com>
2025-01-24 21:18:16 +0100

    fix: correct division operation in list_division function to use list indices

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -4,7 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
     result = 0
     for i in range(list_length):
         try:
-            result = my_list_1 / my_list_2
+            result = my_list_1[i]/ my_list_2[i]
         except ZeroDivisionError:
             print("wrong type")
         except TypeError:
