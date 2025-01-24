493940a79aced24b8c5aa818f41637e95164c18c
Myriam <myriiamwayne@gmail.com>
2025-01-24 18:02:30 +0100

    fix: update best_score function to correctly check for an empty dictionary

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == 0:
+    if not a_dictionary:
         return None
     for key, value in a_dictionary.items():
         keymax = max(a_dictionary)   
