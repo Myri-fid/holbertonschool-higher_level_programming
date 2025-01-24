ccffaaa83eb7cfccb0e11ad83bfafd1f0ba19940
Myriam <myriiamwayne@gmail.com>
2025-01-24 18:01:29 +0100

    feat: implement best_score function to find the key with the highest value in a dictionary

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == 0:
+        return None
+    for key, value in a_dictionary.items():
+        keymax = max(a_dictionary)   
+    return keymax
