9e33742cb0b24edbbbe955b00bc0ace58e62c116
Myriam <myriiamwayne@gmail.com>
2025-01-29 15:42:24 +0100

    feat: implement Square class with size attribute and update main script

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module de documentation pour 0-square.py
+"""
+
+
+class Square:
+    """
+    Dcumentation de la class Square.
+    """
+
+    def __init__(self, size):
+        """
+        Initialisation class Square.
+        Initialise l'attribut d'instance size.
+        """
+        self.__size = size
