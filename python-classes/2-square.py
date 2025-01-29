408d5894087a7474fe168acb97f6ae5c91d2d7d0
Myriam <myriiamwayne@gmail.com>
2025-01-29 16:50:09 +0100

    fix: improve formatting and consistency in Square class initialization

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,13 +9,13 @@ class Square:
     Dcumentation de la class Square.
     """
 
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """
         Initialisation class Square.
         Initialise l'attribut d'instance size.
         """
-        if not isinstance (size, int):
-            raise TypeError ("size must be an integer")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError ("size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
