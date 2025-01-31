71bb075f62f5421b1b451023f66e19ebbacfa7d0
Myriam <myriiamwayne@gmail.com>
2025-01-31 19:47:22 +0100

    fix: update size method documentation and validation in Square class

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -15,20 +15,31 @@ class Square:
         Initialise l'attribut d'instance size.
         """
 
-
         self.__size = size
 
     def size(self):
+        """
+        Initialisation class Square.
+        Initialise l'attribut d'instance size.
+        """
+
         return self.__size
-    
+
     def size(self, value):
-        if not isinstance(size, int):
+        """
+        Initialisation class Square.
+        Initialise l'attribut d'instance size.
+        """
+
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
     def area(self):
         """
         Initialisation class Square.
         Initialise l'attribut d'instance size.
         """
+
         return self.__size ** 2
