3bf796f71bc2bb7b4185958f8ca5db3de6077b24
Myriam <myriiamwayne@gmail.com>
2025-01-31 22:53:24 +0100

    fix: update documentation and correct method implementation in Rectangle class

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """
-Module de documentation pour rectangle
+Module de documentation pour rectangle.py
 """
-
 
 class Rectangle:
     """
-    Class rectangle.
+    Classe qui définit un rectangle.
     """
 
     def __init__(self, width=0, height=0):
         """
-        Initialisation de la class Rectangle.
-
+        Initialisation de la classe Rectangle.
         """
         self.width = width
         self.height = height
@@ -20,14 +18,14 @@ class Rectangle:
     @property
     def width(self):
         """
-        def widht self
+        attribut width
         """
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-        def widht self
+        attribut width
         """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -38,14 +36,14 @@ class Rectangle:
     @property
     def height(self):
         """
-        def height self
+        attribut height
         """
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        def height self
+        attribut height
         """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
@@ -55,14 +53,14 @@ class Rectangle:
 
     def area(self):
         """
-        def area self
+        surface du rectangle.
         """
         return self.__width * self.__height
 
     def perimeter(self):
         """
-        def perimetre self
+        périmètre du rectangle.
         """
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__length + self.__width)
+        return 2 * (self.__width + self.__height)
