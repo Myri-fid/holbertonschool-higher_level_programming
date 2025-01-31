c69e6ba6c3bbd030ba71ac63506222d93053a4e4
Myriam <myriiamwayne@gmail.com>
2025-01-31 22:45:20 +0100

    feat: implement Rectangle class with width and height properties, including validation and area calculation methods

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Module de documentation pour rectangle
+"""
+
+
+class Rectangle:
+    """
+    Class rectangle.
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Initialisation de la class Rectangle.
+
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        def widht self
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        def widht self
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        def height self
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        def height self
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        def area self
+        """
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.length + self.width)
