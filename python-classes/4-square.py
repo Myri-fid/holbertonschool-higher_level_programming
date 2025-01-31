c539b9ff5244601bc1865cc56df25b35f3c29423
Myriam <myriiamwayne@gmail.com>
2025-01-31 19:38:54 +0100

    feat: implement Square class with size validation and update main script for demonstration

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module de documentation pour 0-square.py
+"""
+
+
+class Square:
+    """
+    Dcumentation de la class Square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialisation class Square.
+        Initialise l'attribut d'instance size.
+        """
+
+
+        self.__size = size
+
+    def size(self):
+        return self.__size
+    
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+    def area(self):
+        """
+        Initialisation class Square.
+        Initialise l'attribut d'instance size.
+        """
+        return self.__size ** 2
