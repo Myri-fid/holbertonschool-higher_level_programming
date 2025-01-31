a13a653a297298c657cf22a960491020a47824db
Myriam <myriiamwayne@gmail.com>
2025-01-31 22:23:33 +0100

    feat: update Rectangle class to include property methods for width and height

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,22 +6,24 @@ Documentation
 
 class Rectangle:
     """
-Rectangle class empty
-"""
+    Rectangle class empty
+    """
 
     def __init__(self, width=0, height=0):
         """
         Initialisation de la classe Rectangle.
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
+    @property
     def width(self):
         """
         Initialisation de la classe Rectangle.
         """
         return self.__width
 
+    @property
     def width(self, value):
         """
         Initialisation de la classe Rectangle.
@@ -32,12 +34,14 @@ Rectangle class empty
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
     def height(self):
         """
         Initialisation de la classe Rectangle.
         """
         return self.__height
 
+    @property
     def height(self, value):
         """
         Initialisation de la classe Rectangle.
