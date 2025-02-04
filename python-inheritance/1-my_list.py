f3e4cc0471c734ffd7225839baa612655c210e97
Myriam <myriiamwayne@gmail.com>
2025-02-04 14:16:54 +0100

    feat: add docstring to MyList class and create new is_same_class module

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 class MyList(list):
+    """
+    A class that inherits from the list and
+    adds a method to print the sorted list.
+    """
+
     def print_sorted(self):
         """
         that prints the list, but sorted (ascending sort)
