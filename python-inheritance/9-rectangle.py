2f5cc7855063bf3c210287e0dec0ea49e70e1f2f
Myriam <myriiamwayne@gmail.com>
2025-02-07 16:43:52 +0100

    fix: remove unnecessary blank lines in BaseGeometry class

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -13,7 +13,7 @@ class BaseGeometry:
         """
         Raise an exception because area is not implemented
         """
-    
+
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
@@ -21,11 +21,12 @@ class BaseGeometry:
         Validate value as an integer greater than 0
         name: string
         """
-    
+
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+
 
 """
     class Rectangle that inherits from BaseGeometry.
