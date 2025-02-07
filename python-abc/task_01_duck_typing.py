0a6d684029077a0f7b5bf83165f37b00627acf0b
Myriam <myriiamwayne@gmail.com>
2025-02-07 19:50:18 +0100

    feat: implement Rectangle class with area and perimeter methods

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -22,11 +22,15 @@ class Circle(Shape):
         return 2 * math.pi * self.radius
 
 class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
     def area(self):
-        pass
+        return self.width * self.height
 
     def perimeter(self):
-        pass
+        return 2 * (self.width + self.height)
 
 def shape_info(shape):
     pass
