74b328796270613586e9474adee2ed8aa4cde8b1
Myriam <myriiamwayne@gmail.com>
2025-02-07 13:38:49 +0100

    feat: refine inherits_from function to correctly check inheritance and improve readability

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,13 +3,12 @@
     an instance of a class that inherited (directly or indirectly)
     from the specified class ; otherwise False.
 """
+
+
 def inherits_from(obj, a_class):
     """  function that returns True if the object is
     an instance of a class that inherited (directly or indirectly)
     from the specified class ; otherwise False.
     """
 
-    if isinstance(obj, a_class):
-        return True
-    else:
-        False
+    return isinstance(obj, a_class) and type(obj) is not a_class
