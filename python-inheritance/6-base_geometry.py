7419929fc3c08963c9594390d58b3eac2eda66eb
Myriam <myriiamwayne@gmail.com>
2025-02-07 13:56:35 +0100

    feat: implement BaseGeometry class with area method and update main script for demonstration

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+an empty class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    an empty class BaseGeometry.
+    """
+
+    def area(self):
+        raise Exception("area() is not implemented")
