8c8b7c55925c085bfe4e8868ae67b119f6236126
Myriam <myriiamwayne@gmail.com>
2025-02-07 16:35:36 +0100

    feat: refactor Rectangle class to use super() for integer validation

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -13,7 +13,7 @@ class BaseGeometry:
         """
         Raise an exception because area is not implemented
         """
-
+    
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
@@ -21,18 +21,18 @@ class BaseGeometry:
         Validate value as an integer greater than 0
         name: string
         """
-
+    
         if not isinstance(value, int):
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
-    """
+"""
     class Rectangle that inherits from BaseGeometry.
-    """
+"""
 
 
-class Rectangle:
+class Rectangle(BaseGeometry):
     """
     class Rectangle that inherits from BaseGeometry.
     """
@@ -42,7 +42,7 @@ class Rectangle:
         class Rectangle that inherits from BaseGeometry
         """
 
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
+        super().integer_validator("width", width)
         self.__width = width
+        super().integer_validator("height", height)
         self.__height = height
