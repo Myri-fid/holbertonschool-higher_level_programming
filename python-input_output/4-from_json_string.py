b20d7cfd92599d62e463e5ec931501f08c4efbbf
Myriam <myriiamwayne@gmail.com>
2025-02-10 17:52:55 +0100

    feat: implement from_json_string function to deserialize a JSON string into a Python object

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import json
+"""
+a function that returns an object (Python data structure)
+represented by a JSON string
+"""
+
+
+def from_json_string(my_str):
+    """
+    a function that returns an object (Python data structure)
+    represented by a JSON string:
+    """
+
+    return json.loads(str)
