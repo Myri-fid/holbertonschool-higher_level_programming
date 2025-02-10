8c30aef85d251cb885c377129497c67ccc675273
Myriam <myriiamwayne@gmail.com>
2025-02-10 14:58:43 +0100

    feat: add docstring and print functionality to read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+ a function that reads a
+ text file (UTF8) and prints it to stdout
+"""
+
+
 def read_file(filename=""):
+    """
+    a function that reads a
+    text file (UTF8) and prints it to stdout
+    """
+
     with open('my_file_0.txt', encoding="utf-8") as f:
         read_data = f.read()
-    f.read 
+    print(read_data)
