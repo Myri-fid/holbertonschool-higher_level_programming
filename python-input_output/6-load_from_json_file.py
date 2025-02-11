7caf2436d3f02ecb67dd72fe020e741ff543b329
Myriam <myriiamwayne@gmail.com>
2025-02-11 13:18:26 +0100

    doc: add docstring to load_from_json_file function for clarity

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+ a function that creates an Object from a “JSON file”
+"""
 def load_from_json_file(filename):
+    """
+     a function that creates an Object from a “JSON file”
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         number_char = f.read()
     return number_char
