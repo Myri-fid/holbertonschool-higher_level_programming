2d2518d54f0c69b3c98977af64f797ec773f3a5c
Myriam <myriiamwayne@gmail.com>
2025-02-11 17:40:12 +0100

    fix: change file opening mode to default in load_from_json_file function

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -13,5 +13,5 @@ def load_from_json_file(filename):
     """
      a function that creates an Object from a “JSON file”
     """
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, 'r') as f:
         return json.load(f)
