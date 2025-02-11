f2d66ad2d015319e54fc8c23ead6806ffa2a27c9
Myriam <myriiamwayne@gmail.com>
2025-02-11 18:32:37 +0100

    fix: specify file opening mode as 'r' in read_file function

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -11,5 +11,5 @@ def read_file(filename=""):
     text file (UTF8) and prints it to stdout
     """
 
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read())
