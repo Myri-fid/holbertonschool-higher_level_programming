1bccf72e8e67b9d6ec400c5454744507d95b025b
Myriam <myriiamwayne@gmail.com>
2025-02-11 13:50:29 +0100

    fix: update load_from_json_file to deserialize JSON data and improve function docstring

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,10 +2,16 @@
 """
  a function that creates an Object from a “JSON file”
 """
+
+import json
+"""
+ a function that creates an Object from a “JSON file”
+"""
+
+
 def load_from_json_file(filename):
     """
      a function that creates an Object from a “JSON file”
     """
     with open(filename, 'r', encoding="utf-8") as f:
-        number_char = f.read()
-    return number_char
+        return json.load(f)
