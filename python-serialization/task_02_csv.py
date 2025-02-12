17c4a8474da6581b2aa4e2dded6064da114ef84c
Myriam <myriiamwayne@gmail.com>
2025-02-12 17:56:57 +0100

    feat: add docstring to convert_csv_to_json function for clarity

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,9 +1,19 @@
 import csv
 import json
+"""
+ reading data from one format (CSV) and converting
+ it into another format (JSON)
+"""
+
 
 def convert_csv_to_json(csv_filename):
+    """
+    reading data from one format (CSV) and converting
+    it into another format (JSON)
+    """
+
     data = []
-    with open(csv_filename,'r') as file:
+    with open(csv_filename, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             data.append(row)
