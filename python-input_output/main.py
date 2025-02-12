2dcdc2fca1c8bafe350bb041e4c6f368890b3fae
Myriam <myriiamwayne@gmail.com>
2025-02-12 15:00:10 +0100

    feat: add Pascal's triangle implementation and update main.py for printing

--- python-input_output/main.py
+++ python-input_output/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-Student = __import__('9-student').Student
+"""
+12-main
+"""
+pascal_triangle = __import__('12-pascal_triangle').pascal_triangle
 
-students = [Student("John", "Doe", 23), Student("Bob", "Dylan", 27)]
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
 
-for student in students:
-    j_student = student.to_json()
-    print(type(j_student))
-    print(j_student['first_name'])
-    print(type(j_student['first_name']))
-    print(j_student['age'])
-    print(type(j_student['age']))
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))
