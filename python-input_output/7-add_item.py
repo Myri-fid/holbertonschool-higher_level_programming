c0b0b64246ed3aea99de24cb3a96ddf78213646e
Myriam <myriiamwayne@gmail.com>
2025-02-12 12:47:58 +0100

    fix: update file opening modes in add_item.py and improve docstrings

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """
- script that adds all arguments to a Python list, and then save them to a file
+a function that writes an Object to a text
+file, using a JSON representation
+"""
+
+import json
+"""
+a function that writes an Object to a text
+file, using a JSON representation
 """
 
 
@@ -10,14 +17,24 @@ def save_to_json_file(my_obj, filename):
     file, using a JSON representation
     """
 
-    with open(filename, 'w', encoding="utf-8") as f:
+    with open(filename, 'w') as f:
         json.dump(my_obj, f)
+
+"""
+ a function that creates an Object from a “JSON file”
+"""
+
+import json
+"""
+ a function that creates an Object from a “JSON file”
+"""
 
 
 def load_from_json_file(filename):
     """
      a function that creates an Object from a “JSON file”
     """
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, 'r') as f:
         return json.load(f)
 
+save_to_json_file(data, "add_item.json")
