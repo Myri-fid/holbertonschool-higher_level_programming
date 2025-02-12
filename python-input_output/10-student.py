edc8d89bd223ce06df7aec6a1e5c04a7b08c3a69
Myriam <myriiamwayne@gmail.com>
2025-02-12 14:12:57 +0100

    feat: add Student class with JSON serialization support

---
+++ python-input_output/10-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+class Student that defines a student by name, last name et age
+"""
+
+
+class Student:
+    """
+    class Student that defines a student by name, last name et age
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name and age:
+        def __init__(self, first_name, last_name, age)
+        """
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None)
+        """
+        function that returns the dictionary description with simple data
+        structure (list, dictionary, string, integer and boolean) for JSON
+        serialization of an object
+        """
+
+        if attrs is None:
+            return self.__dict__
