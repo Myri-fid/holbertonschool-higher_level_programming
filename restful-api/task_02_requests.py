c46dac378851dc0624932d6fc8c6bbd6e1ce7510
Myriam <myriiamwayne@gmail.com>
2025-02-19 13:52:38 +0100

    feat: add docstrings to fetch_and_print_posts and fetch_and_save_posts functions for clarity

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,14 +1,33 @@
 import requests
 import csv
 
+"""
+ Cette fonction envoie une requête GET à l'API JSONPlaceholder pour récupérer
+une liste de posts.
+"""
+
+
 def fetch_and_print_posts():
+    """
+    Cette fonction envoie une requête GET à l'API
+    JSONPlaceholder pour récupérer
+    une liste de posts.
+    """
+
     url = 'https://jsonplaceholder.typicode.com/posts'
     Response = requests.get(url)
     if Response.status_code == 200:
         todo = Response.json()
         print(todo)
 
+
 def fetch_and_save_posts():
+    """
+    Cette fonction envoie une requête GET à l'API
+    JSONPlaceholder pour récupérer
+    une liste de posts.
+    """
+
     url = 'https://jsonplaceholder.typicode.com/posts'
     Response = requests.get(url)
     if Response.status_code == 200:
