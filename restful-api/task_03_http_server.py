8e7cda891ab872be31f9c3fb490945b57830ffab
Myriam <myriiamwayne@gmail.com>
2025-02-21 01:38:57 +0100

    feat: enhance SimpleHTTPRequestHandler with docstrings for better code clarity

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,19 +1,34 @@
 import http.server
 import json
+"""
+basic web server using the http.server module
+"""
+
 
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
+    """
+    basic web server using the http.server module
+    """
+
     def do_GET(self):
+        """
+        server class to serve a sample JSON data when the endpoint
+        """
+
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        
+
         if self.path == '/':
             self.wfile.write(b'Hello, this is a simple API!')
         elif self.path == '/data':
-            response = json.dumps({"name": "John", "age": 30, "city": "New York"})
+            response = json.dumps({"name": "John",
+                                   "age": 30, "city": "New York"})
             self.wfile.write(response.encode('utf-8'))
         elif self.path == '/info':
-            response = json.dumps({"version": "1.0", "description": "A simple API built with http.server"})
+            response = json.dumps({"version": "1.0",
+                                   "description":
+                                   "A simple API built with http.server"})
             self.wfile.write(response.encode('utf-8'))
         elif self.path == '/status':
             self.wfile.write(b'OK')
@@ -23,8 +38,8 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'Endpoint not found')
 
+
 if __name__ == '__main__':
     httpd = http.server.HTTPServer(('', 8000), SimpleHTTPRequestHandler)
     print('Le serveur démarre sur le port 8000...')
     httpd.serve_forever()
-
