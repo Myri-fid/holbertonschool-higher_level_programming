129899339f80442f4bc547206a31fbbd465a4fb3
Myriam <myriiamwayne@gmail.com>
2025-02-21 01:40:29 +0100

    feat: improve SimpleHTTPRequestHandler to send responses for all defined endpoints

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -15,22 +15,22 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
         server class to serve a sample JSON data when the endpoint
         """
 
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-
         if self.path == '/':
+            self.send_response(200)
             self.wfile.write(b'Hello, this is a simple API!')
         elif self.path == '/data':
+            self.send_response(200)
             response = json.dumps({"name": "John",
                                    "age": 30, "city": "New York"})
             self.wfile.write(response.encode('utf-8'))
         elif self.path == '/info':
+            self.send_response(200)
             response = json.dumps({"version": "1.0",
                                    "description":
                                    "A simple API built with http.server"})
             self.wfile.write(response.encode('utf-8'))
         elif self.path == '/status':
+            self.send_response(200)
             self.wfile.write(b'OK')
         else:
             self.send_response(404)
