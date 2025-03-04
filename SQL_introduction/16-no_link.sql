65dd976adad939d1ee4a00526faa745feccd7192
Myriam <myriiamwayne@gmail.com>
2025-03-04 20:41:29 +0100

    feat: update SQL script to select scores and names from second_table ordered by score

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
