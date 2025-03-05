2583fb93e6f7c7c2e4d8e09caa5c9c66a78efad6
Myriam <myriiamwayne@gmail.com>
2025-03-05 20:58:11 +0100

    feat: add SQL scripts for listing databases and user privileges

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- Créer les utilisateurs et leur accorder des privilèges
+-- Lister les privilèges de user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Lister les privilèges de user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
