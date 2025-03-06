cb625a53840eb679c8238a8e7ba9bf0e181c1504
Myriam <myriiamwayne@gmail.com>
2025-03-06 10:55:28 +0100

    feat: add FLUSH PRIVILEGES command to ensure user privileges are updated

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -5,3 +5,6 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'uSer_0d_1_pwd';
 GRANT ALL PRIVILEGES
 ON *.* TO 'user_0d_1'@'localhost'
 WITH GRANT OPTION;
+
+-- privilege
+FLUSH PRIVILEGES;
