00020241ccccddbe441482bab03fd4fc8bc2b8e7
Myriam <myriiamwayne@gmail.com>
2025-03-06 10:57:48 +0100

    create user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- user_0d_1 password should be set to user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'uSer_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- privilege
 GRANT ALL PRIVILEGES
