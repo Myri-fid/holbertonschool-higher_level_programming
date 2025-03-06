9d17ab52a374738fa401329345a55d3c3739f821
Myriam <myriiamwayne@gmail.com>
2025-03-06 11:03:18 +0100

    create user 2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,10 @@
+-- user_0d_2 password should be set to user_0d_2_pwd
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'uSer_0d_2_pwd';
+
+-- privilege
+GRANT ALL PRIVILEGES
+ON *.* TO 'user_0d_2'@'localhost'
+WITH GRANT OPTION;
+
+-- privilege
+FLUSH PRIVILEGES;
