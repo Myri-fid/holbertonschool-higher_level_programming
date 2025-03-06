9a24e487d58545cedf7dc0b0a665be110fc71a87
Myriam <myriiamwayne@gmail.com>
2025-03-06 15:58:24 +0100

    refactor: update user_0d_1 password comment and remove unnecessary privilege grants

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,10 +1,3 @@
--- user_0d_1 password should be set to user_0d_1_pwd
+-- le mot de passe de user_0d_1 doit être défini sur user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- privilege
-GRANT SELECT
-ON *.* TO 'user_0d_1'@'localhost'
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost'
-
--- privilege
-FLUSH PRIVILEGES;
+GRANT SELECT ON *.* TO 'user_0d_1'@'localhost';
