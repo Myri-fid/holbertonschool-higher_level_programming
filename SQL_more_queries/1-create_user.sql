ec71e47a1cfeabea68cda4368f825d8ea49ac2fd
Myriam <myriiamwayne@gmail.com>
2025-03-06 10:54:32 +0100

    feat: add privileges for user_0d_1 in SQL user creation script

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,7 @@
 -- user_0d_1 password should be set to user_0d_1_pwd
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'uSer_0d_1_pwd';
+
+-- privilege
+GRANT ALL PRIVILEGES
+ON *.* TO 'user_0d_1'@'localhost'
+WITH GRANT OPTION;
