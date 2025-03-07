43c3ca0fe7e3ebcaf891d5181f7747b02ea18b0d
Myriam <myriiamwayne@gmail.com>
2025-03-07 19:24:45 +0100

    feat: update query to list shows with linked genres in tv_shows table

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,4 @@
 --  script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT tv_shows FROM hbtn_0d_tvshows JOIN tv_shows_genres ON hbtn_0d_tvshows (id);
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+
