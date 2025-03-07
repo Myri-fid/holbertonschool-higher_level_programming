b0542752b668737b53816538859478ef018bbb0f
Myriam <myriiamwayne@gmail.com>
2025-03-07 18:39:16 +0100

    fix: remove unnecessary USE statement from California cities query

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,3 @@
 --  lists all the cities of California that can be found
-USE hbtn_0d_usa;
 SET @california_id = (SELECT id FROM states WHERE name = 'California');
 SELECT id, name FROM cities WHERE state_id = @california_id ORDER BY id ASC;
