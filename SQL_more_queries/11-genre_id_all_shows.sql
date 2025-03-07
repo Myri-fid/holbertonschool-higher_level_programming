348b452a5fede4bc58baccf7f38d9ae111bfbb53
Myriam <myriiamwayne@gmail.com>
2025-03-07 19:38:29 +0100

    fix: change join to LEFT JOIN in genre ID query to include shows without genres

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 --  script that lists all shows contained in the database hbtn_0d_tvshows, show doesn’t have a genre, display NULL
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows 
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
