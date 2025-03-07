dbf934345a43b5d9cce34b50050c300e6d8c6912
Myriam <myriiamwayne@gmail.com>
2025-03-07 21:34:46 +0100

    fix: correct password in user creation script for consistency

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- le mot de passe de user_0d_1 doit être défini sur user_0d_1_pwd
-CREATE USER if NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'uSer_0d_1_pwd';
+CREATE USER if NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
