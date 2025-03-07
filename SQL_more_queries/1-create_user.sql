6c02b47f7084251bcd54d735e6ee4872e61babb6
Myriam <myriiamwayne@gmail.com>
2025-03-07 16:25:53 +0100

    fix: remove unnecessary flush command after granting privileges to user_0d_1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- le mot de passe de user_0d_1 doit être défini sur user_0d_1_pwd
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'uSer_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+CREATE USER if NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'uSer_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
