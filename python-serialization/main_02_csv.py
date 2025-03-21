917484f3b2bb4c52a97e588f0db2fa301420e61b
Myriam <myriiamwayne@gmail.com>
2025-03-21 19:42:29 +0100

    feat: enhance CSV to JSON conversion with error handling and improved output format

--- python-serialization/main_02_csv.py
+++ python-serialization/main_02_csv.py
@@ -1,1 +1,6 @@
-Data from data.csv has been converted to data.json
+#!/usr/bin/env python3
+from task_02_csv import convert_csv_to_json
+
+csv_file = "data.csv"
+convert_csv_to_json(csv_file)
+print(f"Data from {csv_file} has been converted to data.json")
--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -12,8 +12,16 @@ def convert_csv_to_json(csv_filename):
     it into another format (JSON)
     """
 
-    data = []
-    with open(csv_filename, 'r') as file:
-        reader = csv.DictReader(file)
-        for row in reader:
-            data.append(row)
+    try:
+        with open(csv_filename, mode='r', encoding='utf-8') as csvfile:
+            csv_reader = csv.DictReader(csvfile)
+            data = [row for row in csv_reader]
+        with open('data.json', mode='w', encoding='utf-8') as jsonfile:
+           json.dump(data, jsonfile, indent=4)
+        return True
+    except FileNotFoundError:
+        print(f"Le fichier {csv_filename} est introuvable.")
+        return False
+    except Exception as e:
+        print(f"Une erreur est survenue : {e}")
+        return False
