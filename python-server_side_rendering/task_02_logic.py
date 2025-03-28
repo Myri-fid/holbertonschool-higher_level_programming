60ab08d1b17f125d5c6c34ff5bde50686bc6a5e0
Myriam <myriiamwayne@gmail.com>
2025-03-28 17:36:38 +0100

    feat: restore task_01_jinja.py with updated routes and reintroduce items.json

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python3
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/about')
-def a_propos():
-    return render_template('about.html')
-
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
 
 @app.route('/items')
 def items():
     with open('items.json', 'r') as file:
         data = json.load(file)
-    return render_template('items.html', items=data['items'])
+    return render_template('items.html', items=data.get("items", []))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
