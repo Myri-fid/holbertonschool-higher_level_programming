ebf4875be164ca7acb19180a7f9abf476c53a173
Myriam <myriiamwayne@gmail.com>
2025-04-01 14:20:17 +0200

    fix: update print_square function to improve input validation and output formatting

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,9 +5,10 @@
 def print_square(size):
     """  function that prints a square with the character #"""
 
-    if type(size) != int or isinstance(size, float):
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    
-    print(("#" * size + "\n") * size, end="")
+
+    for _ in range(size):
+        print('#' * size)
