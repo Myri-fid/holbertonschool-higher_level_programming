58b989d4ed5a06bcf9bb9be9dc932e92f19ec84f
Myriam <myriiamwayne@gmail.com>
2025-04-01 17:09:26 +0200

    feat: add list_states function to connect to MySQL and retrieve states

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This function connects to the MySQL """
+import MySQLdb
+import sys
+
+
+def list_states():
+    """This function connects to the MySQL """
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM states WHERE BINARY name = '{}'" \
+        "ORDER BY id ASC".format(sys.argv[4])
+    cursor.execute(query)
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    list_states()
