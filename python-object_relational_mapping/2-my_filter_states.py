1ae0834460620ea0649b502a57ab030dacf74217
Myriam <myriiamwayne@gmail.com>
2025-04-01 16:24:36 +0200

    feat: implement state listing functionality with MySQL connection

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This function connects to the MySQL """
+import MySQLdb
+import sys
+
+
+def list_states():
+    """This function connects to the MySQL """
+
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    cursor = conn.cursor()
+    cursor.execute(query)
+
+    query = "SELECT * FROM states WHERE name = '{}'" \
+        "ORDER BY id ASC".format(sys.argv[4])
+
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    list_states()
