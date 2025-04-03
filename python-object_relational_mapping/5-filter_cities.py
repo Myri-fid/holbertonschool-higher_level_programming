9da4ecd58fcdeb23f2ac67d1d46af3e62773b215
Myriam <myriiamwayne@gmail.com>
2025-04-03 18:18:41 +0200

    feat: add functionality to fetch and display Star Wars character and movies

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""This function connects to the MySQL """
 import MySQLdb
 import sys
 
+
 def list_cities_by_state():
+    """This function connects to the MySQL """
+
     conn = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -12,4 +16,21 @@ def list_cities_by_state():
     )
 
     cursor = conn.cursor()
-    
+    query = """
+        SELECT cities.name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+    """
+    cursor.execute(query)
+    cities = cursor.fetchall()
+    for city in cities:
+        print(city)
+
+    cursor.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    list_cities_by_state()
