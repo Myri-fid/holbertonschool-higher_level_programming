b8f39720752992496d23e2d7b556c69da9d5cb04
Myriam <myriiamwayne@gmail.com>
2025-04-03 16:13:37 +0200

    feat: add toggle functionality to change header color between red and green

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,11 @@
+const toggleButton = document.getElementById('toggle_header');
+toggleButton.addEventListener('click', () => {
+    const header = document.querySelector('header');
+    if (header.classList.contains('red')) {
+        header.classList.remove('red');
+        header.classList.add('green');
+    } else {
+        header.classList.remove('green');
+        header.classList.add('red');
+    }
+});
