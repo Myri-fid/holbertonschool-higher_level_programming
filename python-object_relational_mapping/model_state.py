18f8b1245ac177ed0d8aa1b90fb28b0f77ae371b
Myriam <myriiamwayne@gmail.com>
2025-04-04 18:20:07 +0200

    feat: add class definition for State and improve code formatting

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
+"""python file that contains the class definition of a State"""
+
 
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 import sys
 
+
 Base = declarative_base()
 
+
 class State(Base):
+    """python file that contains the class definition of a State"""
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
 
+
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2],
+                                  sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
